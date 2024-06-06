ad1605c73d5c2cf9108fae4d179bd37b82499979
Legion204 <ri46801@gmail.com>
2024-06-06 19:16:21 +0600

    added delete article funtion for my articles page

--- src/Pages/My_articles/MyArticles.jsx
+++ src/Pages/My_articles/MyArticles.jsx
@@ -3,18 +3,45 @@ import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import useAuth from "../../Hooks/useAuth";
 import SectionTitle from "../../shared/SectionTitle/SectionTitle";
 import { MdDelete, MdEdit } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const MyArticles = () => {
     const { firebaseUser } = useAuth();
     const axiosPublic = useAxiosPublic();
 
-    const { data: myArticles = [] } = useQuery({
+    const { data: myArticles = [], refetch } = useQuery({
         queryKey: ['myArticles'],
         queryFn: async () => {
             const res = await axiosPublic.get(`/my_articles?email=${firebaseUser?.email}`)
             return res.data
         }
     });
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosPublic.delete(`/my_article/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
 
     return (
         <div>
@@ -40,11 +67,11 @@ const MyArticles = () => {
                                 myArticles?.map((myArticle, index) => <tr key={myArticle?._id}>
                                     <th>{index + 1}</th>
                                     <td>{myArticle?.articleTitle}</td>
-                                    <td><button className="btn text-lg bg-red-700 text-white">Details</button></td>
+                                    <td><button className="btn bg-red-700 text-white">Details</button></td>
                                     <td>{myArticle?.status}</td>
                                     <td>{myArticle?.isPremium && "Yes" || "No"}</td>
-                                    <td><button className="btn text-3xl bg-red-700 text-white"><MdEdit /></button></td>
-                                    <td><button className="btn text-3xl bg-red-700 text-white"><MdDelete /></button></td>
+                                    <td><button className="btn text-2xl bg-red-700 text-white"><MdEdit /></button></td>
+                                    <td><button onClick={() => { handleDelete(myArticle?._id) }} className="btn text-2xl bg-red-700 text-white"><MdDelete /></button></td>
                                 </tr>)
                             }
                         </tbody>
